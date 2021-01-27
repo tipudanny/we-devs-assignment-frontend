75cd53f99a066979afef04d8a06b70af7f710e4a
Tipu <tipu.dany@gmail.com>
2021-01-27 11:44:07 +0600

    first commit

---
+++ src/components/Products/AllProduct.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p>
+            For a guide and recipes on how to configure / customize this project,<br>
+            check out the
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ViewProduct',
+}
+</script>
+
